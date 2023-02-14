9c9cd2af072373fe7c2c3c72e877932109b20034
luix gabriel <luisgasbriel@gmail.com>
2023-02-14 09:23:57 -0300

    new ApiKey

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,5 @@
 export const baseURL = 'https://br1.api.riotgames.com/'
-export const apiKey = '?api_key=RGAPI-cd789cba-de3d-49ef-ba3a-17c09781aae0'
+export const apiKey = '?api_key=RGAPI-cd65b578-e6a4-45ec-bd56-2df00c254e40'
 export const summonerREQ = 'lol/summoner/v4/summoners/by-name/'
 
 
-
-// 'https://br1.api.riotgames.com/tft/league/v1/entries/by-summoner/R-U27LQQRdxRg9_B9yh4uibG7kPqr2FxN7q6IGeyMNIje64?api_key=RGAPI-cd789cba-de3d-49ef-ba3a-17c09781aae0'
-
-
-// https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/luixgabriel?api_key=RGAPI-cd789cba-de3d-49ef-ba3a-17c09781aae0
