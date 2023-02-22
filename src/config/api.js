0177cf9dbb5574821065a3d5d4c1694467ac9014
luix gabriel <luisgasbriel@gmail.com>
2023-02-22 14:18:28 -0300

    new ApiKey

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 export const baseURL = 'https://br1.api.riotgames.com/';
-export const apiKey = '?api_key=RGAPI-0c5132b8-882e-469d-a2a6-05cdde75ad08';
+export const apiKey = '?api_key=RGAPI-a658e9c4-42ed-4b07-8ce6-7c11770fa360';
 
 
